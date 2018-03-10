7189331bc41ea36f27073c482b2abba7c5104193
naronA <otherthan.new@gmail.com>
2018-03-10 16:04:27 +0900

    COMPOSER_HOMEの設定がおかしくなっていたので修正する

--- vim/rc/language-server.sh
+++ vim/rc/language-server.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-bash install.sh
-
 ### setup php language sever ###
 composer global config minimum-stability dev
 composer global config prefer-stable true
 composer global require felixfbecker/language-server
-composer global run-script --working-dir="$COMPOSER_HOME/vendor/felixfbecker/language-server parse-stubs"
+composer global run-script --working-dir="$COMPOSER_HOME/vendor/felixfbecker/language-server" parse-stubs
 
 ### setup javascript language sever ###
 npm install -g javascript-typescript-langserver
