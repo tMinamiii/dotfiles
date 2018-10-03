3811a885f7864b344a069c809fb3c7935192b933
naronA <otherthan.new@gmail.com>
2018-10-03 20:16:29 +0900

    tslint prettier

---
+++ sandbox/sh/curl.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+## Create Auth Cookie
+## 認証情報をcookie.txtに出力する
+curl -c cookie.txt localhost:28080/Controller.php -d "act=mg_login" -d "login_id=admin" -d "login_pass=passpass"
+
+## Post Template Data
+function deploy_web_basic_template () {
+    ## cookie.txtを読み込んで、テンプレートをPOSTでアップロードする
+    curl -b cookie.txt localhost:28080/Controller.php -F "act=mg_w_design_edit_exec" -F "id=$1" -F "part=<$2"
+}
+
+function deploy_web_widget_template () {
+    curl -b cookie.txt localhost:28080/Controller.php -F "act=mg_w_design_widget_edit_exec" -F "id=$1" -F "widget_id=$1" -F "part=<$2"
+}
+
+deploy_web_basic_template(1000, )
+
