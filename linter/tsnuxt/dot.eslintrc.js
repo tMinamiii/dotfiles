d68c89e84768a366f5fef6616b7dc3d3cdf62d17
naronA <otherthan.new@gmail.com>
2019-01-14 00:40:35 +0900

    prettierを理解した

--- linter/tsnuxt/dot.eslintrc.js
+++ linter/tsnuxt/dot.eslintrc.js
@@ -9,61 +9,110 @@ module.exports = {
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
-    // parser: 'babel-eslint',
     parser: 'typescript-eslint-parser',
-    ecmaVersion: 2017,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
+    jsx: true,
+    useJSXTextNode: true
+    // parser: 'babel-eslint',
+    // sourceType: 'module',
+    // ecmaVersion: 2017,
+    // ecmaFeatures: {
+    //   jsx: true
+    // }
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended',
-    'standard',
-    'typescript'
+  extends: ['eslint:recommended', 'plugin:vue/recommended', 'plugin:prettier/recommended', 'standard', 'typescript'],
+  plugins: [
+    'vue',
+    'import'
+    // 'prettier',
   ],
-  plugins: ['vue', 'import', 'prettier'],
   rules: {
-    'prettier/prettier': 'off', // フォーマットが壊れる要調査 0にして無視する
-    'typescript/no-var-requires': 'off',
-    'require-jsdoc': 'off',
-
-    'vue/component-name-in-template-casing': 'off',
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/max-attributes-per-line': ['error', {
-      singleline: 3,
-      multiline: {
-        max: 1,
-        allowFirstLine: false
-      }
-    }],
-
-    'import/newline-after-import': ['error', {
-      count: 1
-    }],
-
-    'max-len': ['error', {
-      code: 120,
-      tabWidth: 2,
-      ignoreTrailingComments: true,
-      ignoreUrls: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-      ignoreRegExpLiterals: true
-    }],
+    // 'typescript/no-var-requires': 'off',
+    // フォーマットが壊れる要調査 0にして無視する
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        useTabs: false
+        // parser: 'vue',
+      }
+    ],
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always'
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 5,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any'
+        }
+      }
+    ],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    'import/newline-after-import': [
+      'error',
+      {
+        count: 1
+      }
+    ],
     'no-spaced-func': 'error',
     'prefer-const': 'error', // 再代入がない限り const を強制
-    'semi-spacing': ['error', {
-      after: true,
-      before: false
-    }],
+    'semi-spacing': [
+      'error',
+      {
+        after: true,
+        before: false
+      }
+    ],
     'semi-style': ['error', 'last'],
     'space-before-function-paren': ['error', 'never'],
     semi: ['error', 'always'], // 末尾セミコロンを強制
-    strict: 'off' // strict 強制を緩和
+    strict: 'off', // strict 強制を緩和
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        tabWidth: 2,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true
+      }
+    ]
   }
 };
