cc186549b6124d42812a0a40714c72a94dda0e48
naronA <otherthan.new@gmail.com>
2019-01-16 23:45:46 +0900

    prettier

---
+++ linter/tsnuxt/dot.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  useTabs: false
+  // parser: 'vue',
+};
