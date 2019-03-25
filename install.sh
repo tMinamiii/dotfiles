de5b813173a4492fb5c6f95ffed2c30556c8c4d7
naronA <otherthan.new@gmail.com>
2019-03-25 11:10:04 +0900

    prompt対応

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ ln -s "$HOME/dotfiles/zsh/zshenv" "$HOME/.zshenv"
 ln -s "$HOME/dotfiles/vim" "$HOME/.vim"
 ln -s "$HOME/dotfiles/vim" "$HOME/.config/nvim"
 
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 cargo install ripgrep
 
 ###### tmux ######
