afbcaa0c87f8dd55e03dffc78ce9e75138326e45
tMinamiii <otherthan.new@gmail.com>
2020-02-09 00:59:56 +0900

    update

--- templates/python/install.sh
+++ templates/python/install.sh
@@ -3,19 +3,19 @@ set -Ceu
 
 if [ ! -e "/.vscode" ]; then
   mkdir .vscode
-  curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.vscode/settings.json -o .vscode/settings.json
+  curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.vscode/settings.json -o .vscode/settings.json
 fi
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.env.workspace
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.envrc
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.flake8
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.gitignore
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.isort.cfg
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.mypy.ini
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.pydocstyle
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.pre-commit-config.yaml
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.env.workspace
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.envrc
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.flake8
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.gitignore
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.isort.cfg
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.mypy.ini
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.pydocstyle
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.pre-commit-config.yaml
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/requirements.txt
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/editorconfig
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/pyproject.toml
 python -m venv .venv
 poetry config --local virtualenvs.in-project true
 poetry add -D $(cat requirements.txt)
