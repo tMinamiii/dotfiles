54e0080421960255ef7cee25b763a46cfd691f02
tMinamiii <otherthan.new@gmail.com>
2020-04-03 00:19:20 +0900

    update script

--- install.sh
+++ install.sh
@@ -57,34 +57,39 @@ ln -s "$HOME/dotfiles/vim" "$HOME/.config/nvim"
 ###### tmux ######
 ln -s "$HOME/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
 # ln -s "$HOME/dotfiles/tmux" "$HOME/.tmux.d"
-##### GUI setup #####
-if echo "$ARGS" | grep -sq "\\-\\-gui"; then
-  ##### font setup ######
-  if uname | grep -sq "Darwin"; then
-    ln -s "$HOME/Dropbox/Fonts/Cica*.ttf" "$HOME/Library/Fonts"
-    fc-cache -vf
-  elif grep -sq "Ubuntu" /etc/os-release; then
-    ln -s "$HOME/Dropbox/Fonts" "$HOME/.fonts"
-    fc-cache -vf
-  elif uname -a | grep -sq "Microsoft"; then
-    ln -s "$HOME/Dropbox/Fonts" "$HOME/.fonts"
-    fc-cache -vf
-  fi
 
-  if grep -sq "Ubuntu" /etc/os-release; then
-    sudo apt-get install gconf2
-    gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
-    UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "HackGen 13"
-    gconftool-2 --get /apps/gnome-terminal/profiles/Default/font
-    gconftool-2 --set --type string /apps/gnome-terminal/profiles/Default/font "HackGen 13"
-    ###### Xsession ######
-    ln -s "$HOME/dotfiles/Xsession/xsessionrc" "$HOME/.xsessionrc"
-
-    ###### xprofile for fcitx ######
-    ln -s "$HOME/dotfiles/xprofile/xprofile" "$HOME/.xprofile"
-
-    ##### xkb setup #####
-    ln -s "$HOME/dotfiles/xkb" "$HOME/.xkb"
-  fi
+if grep -sq "Ubuntu" /etc/os-release; then
+  gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+  UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
+  gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "HackGen 13"
 fi
+
+##### GUI setup #####
+# if echo "$ARGS" | grep -sq "\\-\\-gui"; then
+#   ##### font setup ######
+#   if uname | grep -sq "Darwin"; then
+#     ln -s "$HOME/Dropbox/Fonts/Cica*.ttf" "$HOME/Library/Fonts"
+#     fc-cache -vf
+#   elif grep -sq "Ubuntu" /etc/os-release; then
+#     ln -s "$HOME/Dropbox/Fonts" "$HOME/.fonts"
+#     fc-cache -vf
+#   elif uname -a | grep -sq "Microsoft"; then
+#     ln -s "$HOME/Dropbox/Fonts" "$HOME/.fonts"
+#     fc-cache -vf
+#   fi
+#
+#   if grep -sq "Ubuntu" /etc/os-release; then
+#     gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+#     UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
+#     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "HackGen 13"
+#
+#     ###### Xsession ######
+#     # ln -s "$HOME/dotfiles/Xsession/xsessionrc" "$HOME/.xsessionrc"
+#
+#     ###### xprofile for fcitx ######
+#     # ln -s "$HOME/dotfiles/xprofile/xprofile" "$HOME/.xprofile"
+#
+#     ##### xkb setup #####
+#     # ln -s "$HOME/dotfiles/xkb" "$HOME/.xkb"
+#   fi
+# fi
