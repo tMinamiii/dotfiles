e1bc625242230f08b5d671ab958e83dd10f4bd21
tMinamiii <otherthan.new@gmail.com>
2020-07-09 21:09:42 +0900

    silent curl

--- templates/python/install.sh
+++ templates/python/install.sh
@@ -3,15 +3,15 @@ set -Ceu
 
 if [ ! -e "/.vscode" ]; then
   mkdir .vscode
-  curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.vscode/settings.json -o .vscode/settings.json
+  curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.vscode/settings.json -o .vscode/settings.json
 fi
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.envrc
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.gitignore
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/setup.cfg
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.pre-commit-config.yaml
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/requirements.txt
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/editorconfig
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/pyproject.toml
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.envrc
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.gitignore
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/setup.cfg
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.pre-commit-config.yaml
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/requirements.txt
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/editorconfig
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/pyproject.toml
 python -m venv .venv
 poetry config --local virtualenvs.in-project true
 poetry add -D $(cat requirements.txt)
