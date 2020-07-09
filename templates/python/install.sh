130a620bbda66b7a6ab21178ded756af5381224b
tMinamiii <otherthan.new@gmail.com>
2020-07-09 21:08:04 +0900

    pysetup

--- templates/python/install.sh
+++ templates/python/install.sh
@@ -5,13 +5,9 @@ if [ ! -e "/.vscode" ]; then
   mkdir .vscode
   curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.vscode/settings.json -o .vscode/settings.json
 fi
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.env.workspace
 curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.envrc
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.flake8
 curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.gitignore
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.isort.cfg
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.mypy.ini
-curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.pydocstyle
+curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/setup.cfg
 curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/.pre-commit-config.yaml
 curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/requirements.txt
 curl -L -O https://raw.githubusercontent.com/tMinamiii/dotfiles/master/templates/python/editorconfig
