5379fc4267eb19c104aa40672893e52ca80da50f
tMinamiii <otherthan.new@gmail.com>
2020-09-26 20:57:50 +0900

    オレオレ証明書

---
+++ apt.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+    apt-utils \
+    autoconf \
+    automake \
+    bison \
+    build-essential \
+    bzip2 \
+    cmake \
+    cproto \
+    curl \
+    file \
+    g++ \
+    gettext \
+    gettext \
+    git \
+    htop \
+    libacl1-dev \
+    libbz2-dev \
+    libevent-dev \
+    libffi-dev \
+    libgdbm-dev \
+    libgpm-dev \
+    libgtk-3-dev \
+    liblua5.2-0 \
+    liblua5.2-dev \
+    libluajit-5.1-dev \
+    libncurses-dev \
+    libncurses5-dev \
+    libperl-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libtinfo-dev \
+    libtool \
+    libtool-bin \
+    libxmu-dev \
+    libxpm-dev \
+    libyaml-dev \
+    locales \
+    lua5.2 \
+    luajit \
+    make \
+    ncurses-term \
+    ninja-build \
+    p7zip-full \
+    pkg-config \
+    python-dev \
+    python3-dev \
+    ruby-dev \
+    software-properties-common \
+    tmux \
+    tree \
+    unzip \
+    unzip \
+    wget \
+    xz-utils \
+    zlib1g-dev \
+    zsh
