f0befdb66a03d42d36fcf58fc0feb6ca6c2d3751
tMinamiii <otherthan.new@gmail.com>
2021-07-25 15:10:42 +0900

    tools

--- linter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -Ceu
-
-sudo apt install pandoc pandoc-citeproc shellcheck
-pip install black flake8 autopep8 pydocstyle isort mypy vim-vint
-npm i -g markdownlint-cli textlint wsl-open
-GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
-GO111MODULE=on go get golang.org/x/tools/gopls@latest
