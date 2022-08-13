7acb5348153c1effa628fb47041c913e51c1c2d0
tMinamiii <otherthan.new@gmail.com>
2022-08-13 15:49:56 +0900

    update

--- vim/b-init.lua
+++ vim/b-init.lua
@@ -28,7 +28,7 @@ opt.laststatus = 2
 opt.cmdheight = 1
 opt.whichwrap = "b,s,<,>,[,]" -- wrapするカッコ一覧
 opt.list = true -- 不可視文字の可視化
-opt.listchars:append({ tab = [[\|\]], trail = "￭", extends = "❯", precedes = "❮" })
+opt.listchars:append({ tab = [[| ]], trail = "￭", extends = "❯", precedes = "❮" })
 opt.ttimeout = nil
 opt.ttimeoutlen = 10
 opt.scrolloff = 10 -- 3行残して画面スクロールする
@@ -39,6 +39,7 @@ opt.termguicolors = true
 opt.wildmenu = true
 opt.wildmode = "full"
 opt.wildoptions = "pum"
+
 
 -----------------------------------------------------------------------------"
 -- Commands \ Modes | Normal | Insert | Command | Visual | Select | Operator |
@@ -53,15 +54,24 @@ opt.wildoptions = "pum"
 -- imap / inoremap  |    -   |   @    |    -    |   -    |   -    |    -     |
 -- cmap / cnoremap  |    -   |   -    |    @    |   -    |   -    |    -     |
 -----------------------------------------------------------------------------"
-local api = vim.api
-vim.g["&t_8f"] = [[\<Esc>[38;2;%lu;%lu;%lum]]
-vim.g["&t_8b"] = [[\<Esc>[48;2;%lu;%lu;%lum]]
-vim.g["&t_ti."] = [[\e[1 q]]
-vim.g["&t_SI."] = [[\e[5 q]]
-vim.g["&t_EI."] = [[\e[1 q]]
-vim.g["&t_te."] = [[\e[0 q]]
-vim.g.mapleader = [[\<Space>]]
 
+vim.cmd [[
+let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+let &t_ti.="\e[1 q"
+let &t_SI.="\e[5 q"
+let &t_EI.="\e[1 q"
+let &t_te.="\e[0 q"
+]]
+
+vim.g.netrw_liststyle = 1 -- " ファイルツリーの表示形式、1にするとls -laのような表示になります
+vim.g.netrw_banner = 0 -- " ヘッダを非表示にする
+vim.g.netrw_sizestyle = "H" -- " サイズを(K,M,G)で表示する
+vim.g.netrw_timefmt = "%Y/%m/%d(%a) %H:%M:%S" -- " 日付フォーマットを yyyy/mm/dd(曜日) hh:mm:ss で表示する
+vim.g.netrw_preview = 1 -- " プレビューウィンドウを垂直分割で表示する
+vim.g.mapleader = "<Space>"
+
+local api = vim.api
 api.nvim_set_keymap("n", "<ESC><ESC>", ":noh<CR>", { noremap = true, silent = true })
 api.nvim_set_keymap("n", "<M-p>", ":bp<CR>", { noremap = true, silent = true })
 api.nvim_set_keymap("n", "<M-n>", ":bn<CR>", { noremap = true, silent = true })
@@ -75,10 +85,9 @@ api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = true })
 api.nvim_set_keymap("n", "<C-k>", "{", { noremap = true, silent = true })
 api.nvim_set_keymap("n", "<C-j>", "}", { noremap = true, silent = true })
 
---
--- " バックスラッシュやクエスチョンを状況に合わせ自動的にエスケープ
-api.nvim_set_keymap("v", "/", [[getcmdtype() == '/' ? '\/' : '/']], { expr = true, noremap = true })
-api.nvim_set_keymap("v", "?", [[getcmdtype() == '?' ? '\?' : '?']], { expr = true, noremap = true })
+-- バックスラッシュやクエスチョンを状況に合わせ自動的にエスケープ
+vim.cmd [[cnoremap <expr> / getcmdtype() == '/' ? '\/' : '/']]
+vim.cmd [[cnoremap <expr> ? getcmdtype() == '?' ? '\?' : '?']]
 
 --  vim strict mode
 api.nvim_set_keymap("v", "<C-c>", "<nop>", { noremap = true })
@@ -93,6 +102,10 @@ api.nvim_set_keymap("i", "<Down>", "<nop>", { noremap = true })
 api.nvim_set_keymap("i", "<Left>", "<nop>", { noremap = true })
 api.nvim_set_keymap("i", "<Right>", "<nop>", { noremap = true })
 api.nvim_set_keymap("i", "<BS>", "<nop>", { noremap = true })
+api.nvim_set_keymap("i", "<C-h>", "<BS>", { noremap = true })
+api.nvim_set_keymap("i", "<C-l>", "<DEL>", { noremap = true })
+
+api.nvim_set_keymap("t", "<C-j><C-j>", [[<C-\><C-n>]], { noremap = true })
 
 api.nvim_set_keymap("n", "<C-c>", "<nop>", { noremap = true })
 api.nvim_set_keymap("n", "<Up>", "<nop>", { noremap = true })
@@ -101,54 +114,37 @@ api.nvim_set_keymap("n", "<Left>", "<nop>", { noremap = true })
 api.nvim_set_keymap("n", "<Right>", "<nop>", { noremap = true })
 api.nvim_set_keymap("n", "<C-[>", "<nop>", { noremap = true })
 api.nvim_set_keymap("n", "<Esc>", "<nop>", { noremap = true })
-api.nvim_set_keymap("i", "<C-h>", "<BS>", { noremap = true })
-api.nvim_set_keymap("i", "<C-l>", "<DEL>", { noremap = true })
-api.nvim_set_keymap("t", "<C-j><C-j>", [[<C-\><C-n>]], { noremap = true })
 
--- よく押し間違えるので
---[[
-  command! Q :q
-  command! W :w
-  command! Wq :wq
-  command! WQ :wq
-  command! Term :bo terminal ++rows=20
+-- NERDTree
+-- api.nvim_set_keymap("n", "<Leader>n", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
+-- api.nvim_set_keymap("n", "<Leader>h", ":NERDTreeFocus<CR>", { noremap = true, silent = true })
+vim.cmd [[
+nnoremap <silent> <Leader>n :NERDTreeToggle<CR>
+nnoremap <silent> <Leader>h :NERDTreeFocus<CR>
 ]]
+-- easymotion
+-- api.nvim_set_keymap("n", "<Leader>", "<Plug>(easymotion-prefix)", { noremap = true, silent = true })
+api.nvim_create_user_command("Q", ":q", {})
+api.nvim_create_user_command("W", ":w", {})
+api.nvim_create_user_command("Wq", ":wq", {})
+api.nvim_create_user_command("WQ", ":wq", {})
+api.nvim_create_user_command("Term", ":bo terminal ++rows=20", {})
 
--- " ファイルツリーの表示形式、1にするとls -laのような表示になります
-vim.g.netrw_liststyle = 1
--- " ヘッダを非表示にする
-vim.g.netrw_banner = 0
--- " サイズを(K,M,G)で表示する
-vim.g.netrw_sizestyle = "H"
--- " 日付フォーマットを yyyy/mm/dd(曜日) hh:mm:ss で表示する
-vim.g.netrw_timefmt = "%Y/%m/%d(%a) %H:%M:%S"
--- " プレビューウィンドウを垂直分割で表示する
-vim.g.netrw_preview = 1
+vim.cmd [[packadd packer.nvim]]
+vim.cmd [[colorscheme material]]
 
---[[
-if has('terminal')
-  function! s:set_default_ansi_colors() abort
-    if exists('g:terminal_ansi_colors')
-      return
-    endif
+opt.expandtab = true -- タブをスペースにする
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.conceallevel = 2
+opt.concealcursor = "nc"
 
-    let g:terminal_ansi_colors = [
-      \ "#073642", "#c50f1f", "#13a10e", "#c19c00",
-      \ "#3399ff", "#881798", "#3a96dd", "#cccccc",
-      \ "#989898", "#e74856", "#16c60c", "#eadf84",
-      \ "#3b78ff", "#b4009e", "#61d6d6", "#e8e8e8"
-      \ ]
-  endfunction
-  call s:set_default_ansi_colors()
-
-  augroup vimrc
-    autocmd ColorScheme * call s:set_default_ansi_colors()
-  augroup END
-endif
-
-augroup autoreload
-  set autoread
-  autocmd FocusLost,FocusGained,CursorMoved,CursorMovedI,CursorHold,CursorHoldI  * silent! checktime
+vim.cmd [[
+augroup nerdtree_hook_add
+    autocmd FileType nerdtree nmap <buffer> l o
+    autocmd FileType nerdtree nmap <buffer> <C-0> o
+    autocmd FileType nerdtree nmap <buffer> <C-n> j
+    autocmd FileType nerdtree nmap <buffer> <C-p> k
 augroup END
 
 augroup filetypes
@@ -163,11 +159,7 @@ augroup filetypes
   autocmd BufWritePre        * :%s/\r//ge
 augroup END
 
-set expandtab                   " タブをスペースにする
-set tabstop=2
-set shiftwidth=2
-set conceallevel=2
-set concealcursor=nc
+
 augroup indentsize
   autocmd FileType sh         setlocal shiftwidth=2 tabstop=2
   autocmd FileType vue        setlocal shiftwidth=2 tabstop=2
@@ -200,21 +192,20 @@ if $WSL_DISTRO_NAME != ""
         \   'cache_enabled': 1,
         \ }
 endif
-]]
---
--- "if exists('$VIRTUAL_ENV')
--- "  let g:python_host_prog=sort(split(glob($PYENV_ROOT.'/versions/2.7*/bin/python')))[-1]
--- "  let g:python3_host_prog=$VIRTUAL_ENV.'/bin/python'
--- "else
--- "  let g:python_host_prog=sort(split(glob($PYENV_ROOT.'/versions/2.7*/bin/python')))[-1]
--- "  let g:python3_host_prog=sort(split(glob($PYENV_ROOT.'/versions/3*/bin/python')))[-1]
--- "endif
---
---[[
+
+if exists('$VIRTUAL_ENV')
+  let g:python3_host_prog=$VIRTUAL_ENV.'/bin/python'
+else
+  let g:python3_host_prog=sort(split(glob($PYENV_ROOT.'/versions/3*/bin/python')))[-1]
+endif
+
 let s:vim_plug_root = '~/.cache/nvim/plugged'
 let s:vim_plug_plugins = '~/.cache/nvim/plugins'
 set rtp+=~/.cache/nvim/plugged/vim-plug
 set rtp+=~/.cache/nvim/plugins
+if !isdirectory(expand(s:vim_plug_plugins))
+  call system('mkdir -p ' . s:vim_plug_plugins)
+endif
 
 if !isdirectory(expand(s:vim_plug_root))
   echo 'install vim-plug...'
@@ -224,55 +215,255 @@ if !isdirectory(expand(s:vim_plug_root))
 endif
 
 call plug#begin(s:vim_plug_plugins)
-  Plug 'wbthomason/packer.nvim'
-  Plug 'neovim/nvim-lspconfig'
+    Plug 'kaicataldo/material.vim'
+      let g:material_terminal_italics = 0
+      let g:material_theme_style = 'palenight'
+
+   Plug 'buoto/gotests-vim'
+    Plug 'ziglang/zig.vim'
+    Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries', 'for': 'go' }
+      let g:go_highlight_methods = 1
+      let g:go_highlight_structs = 1
+      let g:go_highlight_operators = 1
+      let g:go_highlight_functions = 1
+      let g:go_highlight_function_parameters = 1
+      let g:go_highlight_function_arguments = 1
+      let g:go_highlight_function_calls = 1
+      let g:go_highlight_types = 1
+      let g:go_highlight_fields = 1
+      let g:go_highlight_variable_declarations = 1
+      let g:go_highlight_variable_assignments = 1
+
+      let g:go_echo_command_info = 0
+      let g:go_def_mapping_enabled = 0
+      let g:go_gocode_propose_builtins = 0
+      let g:go_echo_go_info = 0
+
+      let g:go_diagnostics_enabled = 0
+      let g:go_metalinter_autosave = 0
+
+      "" format ""
+      let g:go_fmt_autosave = 0
+      let g:go_term_height = 15
+      let g:go_term_mode = 'on | belowright split'
+      let g:go_term_enabled = 0
+
+      function! s:vim_go_keymap()
+          nnoremap <buffer><silent> <Leader><C-r> :GoRun<CR>
+          nnoremap <buffer><silent> <Leader>rt :GoTestFunc<CR>
+          nnoremap <buffer><silent> <F5> :GoDebugContinue<CR>
+          nnoremap <buffer><silent> <F6> :GoDebugPrint<CR>
+          nnoremap <buffer><silent> <F9> :GoDebugBreakpoint<CR>
+          nnoremap <buffer><silent> <F10> :GoDebugNext<CR>
+          nnoremap <buffer><silent> <F11> :GoDebugStep<CR>
+          nnoremap <buffer><silent> <F12> :GoDebugStop<CR>
+      endfunction
+
+      augroup vim_go_hook_add
+          autocmd FileType go :call s:vim_go_keymap()
+      augroup END
+
+    Plug 'honza/vim-snippets'
+
+    Plug 'liuchengxu/vista.vim'
+      nmap <silent> <C-f><C-v> :<C-u>Vista vim_lsp<CR>
+      let g:vista_close_on_jump = 1
+      let g:vista_icon_indent = ['`-> ', '|-> ']
+      let g:vista#renderer#enable_icon = 0
+      let g:vista#renderer#icons = {
+                  \   'function': 'func',
+                  \   'variable': 'var',
+                  \   'field': 'field',
+                  \   'struct': 'struct',
+                  \   'constant': 'const',
+                  \  }
+
+    Plug 'terryma/vim-multiple-cursors'
+
+    Plug 'osyo-manga/vim-over'
+      nnoremap <silent> <Leader>o :OverCommandLine<CR>
+
+    Plug 'terryma/vim-expand-region'
+      vmap v <Plug>(expand_region_expand)
+      vmap <C-v> <Plug>(expand_region_shrink)
+
+    Plug 'sheerun/vim-polyglot'
+      let g:vim_json_syntax_conceal = 0
+
+    Plug 'tpope/vim-fugitive'
+
+    Plug 'scrooloose/nerdtree'
+      nnoremap <silent> <Leader>n :NERDTreeToggle<CR>
+      nnoremap <silent> <Leader>h :NERDTreeFocus<CR>
+      let g:NERDTreeWinSize = 35
+      let g:NERDTreeLimitedSyntax = 1
+      let g:NERDTreeQuitOnOpen = 0
+      augroup nerdtree_hook_add
+          autocmd FileType nerdtree nmap <buffer> l o
+          autocmd FileType nerdtree nmap <buffer> <C-0> o
+          autocmd FileType nerdtree nmap <buffer> <C-n> j
+          autocmd FileType nerdtree nmap <buffer> <C-p> k
+      augroup END
+
+    Plug 'rhysd/vim-color-spring-night'
+      let g:spring_night_kill_italic = 1
+      let g:spring_night_high_contrast = 1
+
+    Plug 'deris/vim-shot-f'
+
+    Plug 'machakann/vim-highlightedyank'
+
+    Plug 'simeji/winresizer'
+      let g:winresizer_vert_resize = 1
+      let g:winresizer_horiz_resize = 1
+
+    Plug 'Yggdroot/indentLine'
+      let g:indentLine_showFirstIndentLevel = 1
+      let g:indentLine_bufNameExclude = ['_.*', 'NERD_tree.*', '_defx.*']
+      let g:indentLine_bufTypeExluce = ['help', 'nerdtree', 'defx', 'terminal']
+      let g:indentLine_fileTypeExluce = ['help', 'nerdtree', 'defx']
+      let g:indentLine_color_term = 238
+      let g:indentLine_color_gui = '#676767'
+      let g:indentLine_setConceal = 0
+
+    Plug 'andymass/vim-matchup'
+      let g:loaded_matchit = 1
+
+    Plug 'tpope/vim-surround'
+
+    Plug 'easymotion/vim-easymotion'
+    map <Leader> <Plug>(easymotion-prefix)
+
+    Plug 'junegunn/vim-easy-align'
+      nmap ga <Plug>(EasyAlign)
+      xmap ga <Plug>(EasyAlign)
+      let g:easy_align_ignore_groups = []
+
+    Plug 'tomtom/tcomment_vim'
+        vnoremap ? :'<,'>TComment<CR>
+
+    Plug 'previm/previm'
+      if system('uname -a | grep microsoft') != ""
+        let g:previm_open_cmd = '/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe'
+        let g:previm_wsl_mode = 1
+      endif
+
+    Plug 'tyru/open-browser.vim'
+
+    Plug 'ctrlpvim/ctrlp.vim'
+      let g:ctrlp_map = '<C-p>'
+      let g:ctrlp_cmd = 'CtrlP'
+      let g:ctrlp_working_path_mode = 'ra'
+      let g:ctrlp_custom_ignore = {
+        \ 'dir':  '\v[\/]\.(git|hg|svn)$|node_modules',
+        \ 'file': '\v\.(exe|so|dll)$',
+        \ 'link': 'some_bad_symbolic_links',
+        \ }
+    Plug 'mattn/ctrlp-matchfuzzy'
+      let g:ctrlp_match_func = {'match': 'ctrlp_matchfuzzy#matcher'}
+
+    Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+    Plug 'junegunn/fzf.vim'
+      command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview(), <bang>0)
+      command! -bang -nargs=? -complete=dir GFiles call fzf#vim#files(<q-args>, fzf#vim#with_preview(), <bang>0)
+      command! -bang -nargs=* Rg
+          \ call fzf#vim#grep(
+          \ 'rg --column --line-number --hidden --smart-case --no-heading --color=always '.shellescape(<q-args>), 1,
+          \ <bang>0 ? fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}, 'up:60%')
+          \         : fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}, 'right:50%:hidden', '?'),
+          \ <bang>0)
+      let g:fzf_layout = { 'down': '~40%' }
+      nnoremap <silent> <Leader>p :GFiles<CR>
+      nnoremap <silent> <Leader>f m:Files<CR>
+      nnoremap <silent> <Leader>g :Rg<CR>
+      " nnoremap <silent> <Leader>b :Buffers<CR>
+      " nnoremap <silent> <Leader>x :Commands<CR>
+
+    Plug 'rhysd/git-messenger.vim'
+      nmap <Leader>gm <Plug>(git-messenger)
+      let g:git_messenger_include_diff = 'current'
+      let g:git_messenger_always_into_popup = v:true
+      let g:git_messenger_into_popup_after_show = v:true
+
+    Plug 'mattn/emmet-vim'
+
+    Plug 'mechatroner/rainbow_csv', { 'for': 'csv' }
+      let g:rainbow_active = 0
+      let g:rainbow_conf = {
+                  \    'guifgs': ['darkorange2', 'orchid3', 'seagreen3'],
+                  \   'separately': {
+                  \       'nerdtree': 0,
+                  \   },
+                  \ }
+
+    Plug 'plasticboy/vim-markdown', {'for' : ['markdown','mkd']}
+      let g:vim_markdown_conceal = 0
+      let g:vim_markdown_folding_disabled = 1
+
+    Plug 'cohama/lexima.vim'
+    Plug 'prabirshrestha/vim-lsp'
+    Plug 'mattn/vim-lsp-settings'
+    Plug 'prabirshrestha/async.vim'
+    Plug 'prabirshrestha/asyncomplete.vim'
+    Plug 'prabirshrestha/asyncomplete-lsp.vim'
+    Plug 'SirVer/ultisnips'
+    Plug 'thomasfaingnaert/vim-lsp-snippets'
+    Plug 'thomasfaingnaert/vim-lsp-ultisnips'
+      inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+      inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+      inoremap <expr> <CR>    pumvisible() ? asyncomplete#close_popup() : "\<CR>"
+      let g:lsp_settings = {
+            \  'efm-langserver': {
+            \    'disabled': v:false
+            \  },
+            \  'pyls-all': {
+            \    'disabled': v:true
+            \  },
+            \  'pyls': {
+            \    'disabled': v:true
+            \  },
+            \  'pyls-ms': {
+            \    'disabled': v:true
+            \  },
+            \  'jedi-language-server': {
+            \    'disabled': v:true
+            \  },
+            \  'zls': {
+            \    'zip_lib_path': '/usr/loca/zig/lib',
+            \    'zip_exe_path': '/usr/loca/zig/zig'
+            \  }
+            \}
+
+      let g:lsp_diagnostics_enabled = 1
+      let g:lsp_diagnostics_echo_cursor = 1
+      let g:lsp_diagnostics_float_cursor = 0
+      let g:lsp_diagnostics_float_delay = 200
+      let g:lsp_settings_filetype_python = "pyright-langserver"
+      let g:UltiSnipsExpandTrigger="<tab>"
+      let g:UltiSnipsJumpForwardTrigger="<tab>"
+      let g:UltiSnipsJumpBackwardTrigger="<s-tab>"
+
+      function! s:on_lsp_buffer_enabled() abort
+          setlocal omnifunc=lsp#complete
+          setlocal signcolumn=yes
+          if exists('+tagfunc') | setlocal tagfunc=lsp#tagfunc | endif
+          nmap <buffer> gd <plug>(lsp-definition)
+          nmap <buffer> gr <plug>(lsp-references)
+          nmap <buffer> gi <plug>(lsp-implementation)
+          nmap <buffer> gt <plug>(lsp-type-definition)
+          nmap <buffer> <leader>rn <plug>(lsp-rename)
+          nmap <buffer> [g <Plug>(lsp-previous-diagnostic)
+          nmap <buffer> ]g <Plug>(lsp-next-diagnostic)
+          nmap <buffer> K <plug>(lsp-hover)
+          nmap <buffer> <C-l><C-l> <plug>(lsp-document-diagnostics)
+          let g:lsp_format_sync_timeout = 1000
+          autocmd! BufWritePre *.rs,*.go,*.py,*.c,*.ts,*.js call execute('LspDocumentFormatSync')
+      endfunction
+
+      augroup lsp_install
+          au!
+          " call s:on_lsp_buffer_enabled only for languages that has the server registered.
+          autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
+      augroup END
 call plug#end()
 ]]
-
-
-
---
--- " ファイルタイププラグインおよびインデントを有効化
--- syntax on
--- try
---   colorscheme material
--- catch
--- endtry
---
--- hi! MatchParen guibg=#555555 guifg=NONE gui=bold
--- hi! Visual guibg=#888888 guifg=NONE
--- hi! SpecialKey ctermfg=239 ctermbg=NONE
--- hi! clear LineNr
--- hi! clear SignColumn
---
--- hi! Normal guibg=NONE ctermbg=NONE
---
--- hi! NormalFloat    guibg=#334455 guifg=#fffeeb gui=NONE      ctermfg=235  ctermbg=230  cterm=NONE
---
--- hi! Terminal       guibg=#323232 guifg=#fffeeb gui=NONE      ctermfg=235  ctermbg=230  cterm=NONE
--- hi! CursorLine     guibg=NONE    guifg=NONE    gui=underline ctermbg=NONE ctermfg=NONE cterm=underline
--- hi! CursorIM       guibg=#af00af guifg=#000000 gui=NONE      ctermbg=127  ctermfg=16   cterm=NONE
--- hi! HighlightedyankRegion cterm=reverse gui=reverse
---
--- autocmd FileType * call <SID>def_base_syntax() " autocmd Syntax may be better
---
--- function! s:def_base_syntax()
---   " Simple example
---   autocmd FileType python syntax match commonOperator "\(+\|=\|-\|\^\|\*\)"
---   autocmd FileType python syntax match baseDelimiter "\(,\|\.\)"
---   autocmd FileType python hi link commonOperator Operator
---   autocmd FileType python hi link baseDelimiter Special
--- endfunction
---
--- augroup syntaxFromStart
---     autocmd FileType vue :syntax sync fromstart
--- augroup END
---
---
--- if has('multi_byte_ime')
---   highlight Cursor guifg=NONE guibg=Green
---   highlight CursorIM guifg=NONE guibg=Purple
--- endif
---
--- filetype plugin indent on
---
