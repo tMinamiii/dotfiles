873544f5e232d4948d0090db003da8da209431bb
tMinamiii <otherthan.new@gmail.com>
2022-10-07 23:44:05 +0900

    apt

--- apt.sh
+++ apt.sh
@@ -64,3 +64,8 @@ sudo apt-get install -y \
     xz-utils \
     zlib1g-dev \
     zsh
+
+# python
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
