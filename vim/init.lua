9b53aa03bf7fb4a3b209e3a140026f6382376e24
tMinamiii <otherthan.new@gmail.com>
2024-08-22 19:32:49 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -401,8 +401,10 @@ else
     options = {
       icons_enabled = true,
       theme = 'auto',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      -- component_separators = { left = '', right = '' },
+      -- section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
       disabled_filetypes = {
         statusline = {},
         winbar = {},
