be4904f571e592a2a53ce06cea57f10ee3042725
tMinamiii <otherthan.new@gmail.com>
2024-09-01 00:48:42 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -759,7 +759,7 @@ else
   vim.cmd.colorscheme("material")
 
   vim.api.nvim_set_hl(0, 'CocMenuSel', { bg = '#404040' })
-  vim.api.nvim_set_hl(0, 'LineNr', { fg = '#707070' })
+  vim.api.nvim_set_hl(0, 'LineNr', { fg = '#505050' })
 
   -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
   -- vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
