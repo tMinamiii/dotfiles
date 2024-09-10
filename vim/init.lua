3a72437a51529cc759f4489120263784c9471d9b
tMinamiii <otherthan.new@gmail.com>
2024-09-11 01:17:38 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -544,8 +544,24 @@ else
         config = function()
           require("dapui").setup({
             layouts = {
-              { size = 60 },
-              { size = 15 }
+              {
+                elements = {
+                  { id = "scopes",      size = 0.25 },
+                  { id = "breakpoints", size = 0.25 },
+                  { id = "stacks",      size = 0.25 },
+                  { id = "watches",     size = 0.25 }
+                },
+                position = "left",
+                size = 60
+              },
+              {
+                elements = {
+                  { id = "repl",    size = 0.5 },
+                  { id = "console", size = 0.5 }
+                },
+                position = "bottom",
+                size = 15
+              }
             },
           })
         end
