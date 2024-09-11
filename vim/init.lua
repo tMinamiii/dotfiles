46e1cbb306f23c05a69032c9127955bcdb4397ac
tMinamiii <otherthan.new@gmail.com>
2024-09-11 19:40:46 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -636,9 +636,39 @@ else
         end
       }
 
-      use "folke/which-key.nvim"
+      use { "folke/which-key.nvim" }
 
-      use { 'kevinhwang91/nvim-hlslens', config = function() require('hlslens').setup() end }
+      -- use {
+      --   "rcarriga/nvim-notify",
+      --   config = function()
+      --     require("notify").setup({
+      --       background_colour = "#000000"
+      --     })
+      --   end
+      -- }
+
+      -- use {
+      --   "folke/noice.nvim",
+      --   requires = { "MunifTanjim/nui.nvim" }, config = function()
+      --   require("noice").setup({
+      --     lsp = {
+      --       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      --       override = {
+      --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      --         ["vim.lsp.util.stylize_markdown"] = true,
+      --         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      --       },
+      --     },
+      --     -- you can enable a preset for easier configuration
+      --     presets = {
+      --       bottom_search = false,        -- use a classic bottom cmdline for search
+      --       command_palette = true,       -- position the cmdline and popupmenu together
+      --       long_message_to_split = true, -- long messages will be sent to a split
+      --       inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+      --       lsp_doc_border = false,       -- add a border to hover docs and signature help
+      --     },
+      --   })
+      -- end }
 
       use { "nvim-telescope/telescope.nvim", tag = "0.1.8", requires = { "nvim-lua/plenary.nvim" } }
 
