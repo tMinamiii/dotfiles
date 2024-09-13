8251a7c2ef5954d10a4fd5db809dbdaad1806310
tMinamiii <otherthan.new@gmail.com>
2024-09-14 04:12:08 +0900

    updater

--- vim/init.lua
+++ vim/init.lua
@@ -835,17 +835,25 @@ if not g.vscode then
   --------------
   --- vim-go ---
   --------------
-  g.go_highlight_methods = 1
-  g.go_highlight_structs = 1
-  g.go_highlight_operators = 1
-  g.go_highlight_functions = 1
-  g.go_highlight_function_parameters = 1
-  g.go_highlight_function_arguments = 1
-  g.go_highlight_function_calls = 1
-  g.go_highlight_types = 1
+  g.go_highlight_array_whitespace_error = 1
+  g.go_highlight_build_constraints = 1
+  g.go_highlight_chan_whitespace_error = 1
+  g.go_highlight_diagnostic_errors = 1
+  g.go_highlight_diagnostic_warnings = 1
+  g.go_highlight_extra_types = 1
   g.go_highlight_fields = 1
-  g.go_highlight_variable_declarations = 1
+  g.go_highlight_format_strings = 1
+  g.go_highlight_function_calls = 1
+  g.go_highlight_function_parameters = 1
+  g.go_highlight_functions = 1
+  g.go_highlight_generate_tags = 1
+  g.go_highlight_operators = 1
+  g.go_highlight_space_tab_error = 1
+  g.go_highlight_string_spellcheck = 1
+  g.go_highlight_trailing_whitespace_error = 1
+  g.go_highlight_types = 1
   g.go_highlight_variable_assignments = 1
+  g.go_highlight_variable_declarations = 1
 
   g.go_code_completion_enabled = 0
   g.go_def_mapping_enabled = 0
