c01a476071f306bd47b805bba53e39caffa46f8c
tMinamiii <otherthan.new@gmail.com>
2025-03-15 15:03:20 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -662,16 +662,6 @@ else
         end,
       },
       {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-
-        -- For blink.cmp's completion
-        -- source
-        -- dependencies = {
-        --     "saghen/blink.cmp"
-        -- },
-      },
-      {
         "yorickpeterse/nvim-window",
         lazy = false,
         keys = {
