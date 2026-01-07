758817c003b0f7edb6f291f03d07fe0c1b2a5855
t-minami <t-minami@mi-labo.co.jp>
2026-01-07 11:19:34 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -401,7 +401,7 @@ if g.vscode then
         },
         -- stylua: ignore
         keys = {
-          { "<leader>f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+          { "<leader><leader>w", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
           -- { "", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
           -- { "", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
           -- { "<leader>F", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
