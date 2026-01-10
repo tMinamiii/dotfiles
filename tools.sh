218ae54d38f7a1a8b4bae21243d0dc0b906ef0ce
Takahiro Minami <tminamiii@TakahironoMacBook-Air.local>
2026-01-10 21:28:16 +0900

    update

--- tools.sh
+++ tools.sh
@@ -19,7 +19,7 @@ if IsMacOS; then
     ghq \
     htop \
     jq \
-    lua@5.3 \
+    lua \
     luarocks \
     luajit \
     mise \
@@ -38,79 +38,6 @@ if IsMacOS; then
 
   curl https://mise.run | sh
 elif IsUbuntu; then
-<<<<<<< Updated upstream
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get install -y \
-    apt-utils \
-    autoconf \
-    automake \
-    bat \
-    bison \
-    build-essential \
-    bzip2 \
-    cmake \
-    cproto \
-    dconf-cli \
-    direnv \
-    file \
-    g++ \
-    gettext \
-    gh \
-    html-xml-utils \
-    htop \
-    jq \
-    libacl1-dev \
-    libbz2-dev \
-    libevent-dev \
-    libffi-dev \
-    libgdbm-dev \
-    libgpm-dev \
-    libgtk-3-dev \
-    libluajit-5.1-dev \
-    liblua5.1-0 \
-    liblua5.1-0-dev \
-    liblzma-dev \
-    libncurses-dev \
-    libncurses5-dev \
-    libncursesw5-dev \
-    libperl-dev \
-    libreadline-dev \
-    libsqlite3-dev \
-    libssl-dev \
-    libtinfo-dev \
-    libtool \
-    libtool-bin \
-    libxml2-dev \
-    libxmlsec1-dev \
-    libxmu-dev \
-    libxpm-dev \
-    libyaml-dev \
-    llvm \
-    lua5.1 \
-    luarocks \
-    luajit \
-    make \
-    ncurses-term \
-    ninja-build \
-    p7zip-full \
-    pkg-config \
-    python3-dev \
-    ripgrep \
-    ruby-dev \
-    software-properties-common \
-    tk-dev \
-    tmux \
-    tree \
-    unzip \
-    whois \
-    xz-utils \
-    zlib1g-dev \
-    libsixel-bin \
-    libmagickwand-dev \
-    libgraphicsmagick1-dev \
-    zsh
-=======
     sudo apt-get update
     sudo apt-get upgrade -y
     sudo apt-get install -y \
@@ -183,7 +110,6 @@ elif IsUbuntu; then
         libmagickwand-dev \
         libgraphicsmagick1-dev \
         zsh
->>>>>>> Stashed changes
 
   curl https://mise.run | sh
 fi
