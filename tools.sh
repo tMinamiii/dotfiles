8068cf5f5ef88552c17cb029cf77242c9b9e7de6
tMinamiii <otherthan.new@gmail.com>
2026-01-12 18:29:05 +0900

    update

--- tools.sh
+++ tools.sh
@@ -14,14 +14,19 @@ if IsMacOS; then
   brew install \
     bat \
     ctop \
+    curl \
     direnv \
     gh \
     ghq \
+    git \
+    html-xml-utils \
     htop \
+    imagemagick \
     jq \
+    libsixel \
     lua \
-    luarocks \
     luajit \
+    luarocks \
     mise \
     neovim \
     openssl \
@@ -30,86 +35,96 @@ if IsMacOS; then
     sqlite3 \
     tcl-tk \
     tmux \
+    unzip \
     vim \
+    wget \
+    whois \
     xz \
-    libsixel \
-    imagemagick \
     zlib
 
   curl https://mise.run | sh
 elif IsUbuntu; then
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt-get install -y \
-        apt-utils \
-        autoconf \
-        automake \
-        bat \
-        bison \
-        build-essential \
-        bzip2 \
-        cmake \
-        cproto \
-        curl \
-        dconf-cli \
-        direnv \
-        file \
-        g++ \
-        gettext \
-        gh \
-        html-xml-utils \
-        htop \
-        jq \
-        libacl1-dev \
-        libbz2-dev \
-        libevent-dev \
-        libffi-dev \
-        libgdbm-dev \
-        libgpm-dev \
-        libgtk-3-dev \
-        libluajit-5.1-dev \
-        liblua5.1-0 \
-        liblua5.1-0-dev \
-        liblzma-dev \
-        libncurses-dev \
-        libncurses5-dev \
-        libncursesw5-dev \
-        libperl-dev \
-        libreadline-dev \
-        libsqlite3-dev \
-        libssl-dev \
-        libtinfo-dev \
-        libtool \
-        libtool-bin \
-        libxml2-dev \
-        libxmlsec1-dev \
-        libxmu-dev \
-        libxpm-dev \
-        libyaml-dev \
-        llvm \
-        lua5.1 \
-        luarocks \
-        luajit \
-        make \
-        ncurses-term \
-        ninja-build \
-        p7zip-full \
-        pkg-config \
-        python3-dev \
-        ripgrep \
-        ruby-dev \
-        software-properties-common \
-        tk-dev \
-        tmux \
-        tree \
-        unzip \
-        whois \
-        xz-utils \
-        zlib1g-dev \
-        libsixel-bin \
-        libmagickwand-dev \
-        libgraphicsmagick1-dev \
-        zsh
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get install -y \
+    apt-utils \
+    autoconf \
+    automake \
+    bat \
+    bison \
+    build-essential \
+    bzip2 \
+    cmake \
+    cproto \
+    curl \
+    dconf-cli \
+    direnv \
+    file \
+    g++ \
+    gettext \
+    git \
+    html-xml-utils \
+    htop \
+    jq \
+    libacl1-dev \
+    libbz2-dev \
+    libevent-dev \
+    libffi-dev \
+    libgdbm-dev \
+    libgpm-dev \
+    libgraphicsmagick1-dev \
+    libgtk-3-dev \
+    liblua5.1-0 \
+    liblua5.1-0-dev \
+    libluajit-5.1-dev \
+    liblzma-dev \
+    libmagickwand-dev \
+    libncurses-dev \
+    libncurses5-dev \
+    libncursesw5-dev \
+    libperl-dev \
+    libreadline-dev \
+    libsixel-bin \
+    libsqlite3-dev \
+    libssl-dev \
+    libtinfo-dev \
+    libtool \
+    libtool-bin \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libxmu-dev \
+    libxpm-dev \
+    libyaml-dev \
+    llvm \
+    lua5.1 \
+    luajit \
+    luarocks \
+    make \
+    ncurses-term \
+    ninja-build \
+    p7zip-full \
+    pkg-config \
+    python3-dev \
+    ripgrep \
+    ruby-dev \
+    software-properties-common \
+    tk-dev \
+    tmux \
+    tree \
+    unzip \
+    whois \
+    xz-utils \
+    zlib1g-dev \
+    zsh
+  wget
+
+  # install gh
+  type -p curl >/dev/null || sudo apt install curl -y
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+    sudo apt update
+  tools="${tools} gh"
 
   curl https://mise.run | sh
 fi
